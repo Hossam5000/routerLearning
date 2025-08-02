d4163514dc6f5c08393aa1620bb3c1404c6ed9cd
ExceptionPastry HossamHassan <Hossam.Hassan@exceptionpastry.local>
2025-08-02 15:46:26 +0300

    r

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -6,17 +6,20 @@ import cars from "@/data/cars.json";
 // cons & vars
 const route = useRoute();
 const car = cars.find(c => c.id === parseInt(route.params.id));
+
+// code
 </script>
 
 <template>
     <div>
         <h1>Car View</h1>
-        {{ car }}
+        <p>{{ car.name }}</p>
+        <p>{{ car.price }}</p>
+        <p>{{ car.year }}</p>
+
     </div>
 </template>
 
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,16 @@
+<script setup>
+// imports
+
+// const & vars
+
+</script>
+
+<template>
+    <div>
+        <h1>contact info</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
