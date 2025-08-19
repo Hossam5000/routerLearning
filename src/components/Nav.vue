2ac169cdd2751822edde28688a98cc85227f7797
ExceptionPastry HossamHassan <Hossam.Hassan@exceptionpastry.local>
2025-08-19 17:34:06 +0300

    another update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <script setup>
 // imports
 import { RouterLink } from 'vue-router';
-import {ref} from 'vue';
+import { ref } from 'vue';
 
 // cons & vars
 const randomState = ref("");
@@ -10,17 +10,17 @@ const randomState = ref("");
 <template>
     <div>
         {{ randomState }}
-        <button @click="randomState='hello'">click me</button>
+        <button @click="randomState = 'hello'">click me</button>
     </div>
     <div class="nav">
         <RouterLink active-class="active" to="/">Home</RouterLink>
-        <RouterLink active-class="active" to="/about">About</RouterLink>
+        <RouterLink active-class="active" :to="'about'">About</RouterLink>
     </div>
 </template>
 
 <style scoped>
-    .active{
-        color: dodgerblue;
-        font-size: 20px;
-    }
+.active {
+    color: dodgerblue;
+    font-size: 20px;
+}
 </style>
